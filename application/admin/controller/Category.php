277546c2d960f75bb3e8150155f11021286dbef3
muxi <15537261738@163.com>
2018-04-26 01:15:41 +0800

    添加了一些前端静态页面

--- application/admin/controller/Category.php
+++ application/admin/controller/Category.php
@@ -2,32 +2,32 @@
 /**
  * Created by PhpStorm.
  * User: Administrator
- * Date: 2018/4/22 0022
- * Time: 19:41
+ * Date: 2018/4/26 0026
+ * Time: 0:51
  */
 
 namespace app\admin\controller;
+
+
 use think\Controller;
 
-class Book extends Controller
+class Category extends Controller
 {
     public function index(){
         return $this->view->fetch();
     }
-
-    public function borrow(){
+    public function add()
+    {
         return $this->view->fetch();
     }
 
-    public function add(){
-        return $this->view->fetch();
-    }
-    public function update(){
-        return $this->view->fetch();
-    }
-    public function delete(){
+    public function update()
+    {
         return $this->view->fetch();
     }
 
-
+    public function delete()
+    {
+        return $this->view->fetch();
+    }
 }
